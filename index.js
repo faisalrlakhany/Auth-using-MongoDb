bbf849e495b9e612ad43908f2ac5959c35d53358
faisalrlakhany <lakhanyfaisal37@gmail.com>
2025-07-21 16:03:20 +0500

    update index file

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-
 import express from 'express'
-import morgan from 'morgan';
+import morgan from 'morgan'
 import 'dotenv/config'
-import mongoose from 'mongoose';
-import authRouter from './routers/auth.js';
+import mongoose from 'mongoose'
+import authRouter from './routers/auth.js'
 
 const app = express()
-const PORT = 4000;
+const PORT = 4000
 
 app.use(express.json())
 app.use(morgan('tiny'))
 
 // console.log(`MongoDB URI: ${process.env.MONGODBURI}`);
 
-
 // MOngoDB Connection
-mongoose.connect(process.env.MONGODBURI)
-.then(()=> console.log("MongoDB Connected Successfully"))
-.catch((err) => console.error("MongoDB Connection Error:", err))
-
+mongoose
+  .connect(process.env.MONGODBURI)
+  .then(() => console.log('MongoDB Connected Successfully'))
+  .catch(err => console.error('MongoDB Connection Error:', err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use("/auth", authRouter);
+app.use('/auth', authRouter)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on Port${PORT}`);
+  console.log(`Server is running on Port${PORT}`)
 })
-    
